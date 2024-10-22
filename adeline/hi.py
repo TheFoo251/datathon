e9c544eae21e27570ffeb39da52a1908b9a9b227
Adeline Chen <adelinechen@Adelines-MacBook-Air.local>
2024-10-21 20:00:13 -0400

    2000

--- adeline/hi.py
+++ adeline/hi.py
@@ -1,1 +1,16 @@
-print("<3")
+import csv
+import os
+from pathlib import Path
+
+def readfile(fp):
+    data = []
+    with open(Path(fp)) as fin:
+        reader = csv.reader(fin, delimiter=',')
+        for line in reader:
+            data.append(line)
+    return data
+
+
+traffic_crashes = readfile('/Users/adelinechen/CPSC250/datathon/adeline/Traffic_Crashes_20241021.csv')
+
+print(traffic_crashes)
