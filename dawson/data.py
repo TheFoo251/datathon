18091bc46ac4985373d940f10433afa73e85d005
DawsonRichards <66963484+TheFoo251@users.noreply.github.com>
2024-10-22 20:33:23 -0400

    my stuff?

--- dawson/data.py
+++ dawson/data.py
@@ -1,26 +1,30 @@
 import pandas as pd
 from pathlib import Path
-from sklearn import datasets, decomposition
 from sklearn.preprocessing import LabelEncoder
 
-# pca = decomposition.PCA(n_components=3)
-# pca.fit(X)
-# X = pca.transform(X)
 
 # No Injury vs Injury -- 1 vs 0
 
 
 def get_dataframe():
-    data = pd.read_csv(Path("../data.csv"))
+    data = pd.read_csv(Path("data.csv"))
     # replace row with 0 or 1
 
-    encoder = LabelEncoder()
-    data["Injury?"] = encoder.fit_transform(data["Crash Severity"])
+    codes = {
+        "Property Damage Only": 0,
+        "Visible Injury": 1,
+        "Nonvisible Injury": 2,
+        "Severe Injury": 3,
+        "Fatal Injury": 4,
+    }
+    data["Crash Severity"] = data["Crash Severity"].map(codes)
+    data["Severe?"] = data["Crash Severity"] >= 3
     return data
 
 
 if __name__ == "__main__":
     data = get_dataframe()
-    print(data["Injury?"])
     print(data["Crash Severity"].unique())
-    print(list(data))
+    # print(list(data))
+    print(data["Intersection Type"].unique())
+    print(data.groupby("Intersection Type")["Severe?"].value_counts())
