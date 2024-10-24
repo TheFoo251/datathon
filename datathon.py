51cae90a91f412541504926a5602ffdc1c159873
DawsonRichards <66963484+TheFoo251@users.noreply.github.com>
2024-10-23 21:11:19 -0400

    trim file

--- datathon.py
+++ datathon.py
@@ -6,13 +6,6 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# From the dataset, we need the "Intersection Type" for the x-axis, and the ratio of severe/fatal injury crashes to total crashes for the y-axis.
-# Severe injury = "Number of People with Suspected Serious Injury" > 0
-# Fatal injury = "Number of Fatalities" > 0
-
-# Let's group the data by "Intersection Type" and calculate the ratio of severe/fatal crashes to total crashes.
-
-
 def get_summary(df, x_axis):
     # Filter crashes with severe or fatal injuries
     severe_or_fatal = (df["Number of People with Suspected Serious Injury"] > 0) | (
@@ -38,7 +31,6 @@ def get_summary(df, x_axis):
 
 def plot_data(df, x_axis):
     print(df.head())
-    # grouped = grouped.nlargest(10, 'ratio_severe_fatal')
     # Plot
     plt.figure(figsize=(10, 6))
     df["ratio_severe_fatal"].plot(kind="bar", color="skyblue")
@@ -68,20 +60,9 @@ if __name__ == "__main__":
     file_path = "data.csv"
     data = pd.read_csv(file_path)
 
-    # plot_all_features(data)
-
-    # filter by HAMPTON BLVD
-    hampton_blvd_data = data.loc[data["Route or Street Name"] == "HAMPTON BLVD"]
-
-    # count = 0
-    # for letter in hampton_blvd_data["Alcohol Involved"].sum():
-    #     if letter == 'Y':
-    #         count += 1
-
+    # filter Interstate entries
     non_interstate_data = data.loc[
         ~data["Route or Street Name"].str.contains("I-")
         & ~data["Route or Street Name"].str.contains("INTER")
     ]
     plot_all_features(non_interstate_data)
-    # print(non_interstate_data.head())
-    # plot_all_features(hampton_blvd_data)
